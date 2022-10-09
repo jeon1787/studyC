c118f1aaace0c016b1e0dee3a2af0d96d8542a09
eunwoojeon <wjsdmsdn1787@naver.com>
2022-10-09 14:34:14 +0900

    1강 프로젝트 생성 및 빌드 단축키

---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+	return 0;
+
+	/*
+	독하게 시작하는 C 프로그래밍 1강)
+	솔루션 > 프로젝트(한 솔루션에는 복수의 프로젝트가 있을 수 있다.)
+	프로젝트 빌드(proeject build) 단축키 : F7
+	솔루션 빌드 단축키(solution build) : ctrl + shift + B
+	실행(run) 단축키 : ctrl + F5(주의 ctrl 필수 F5키는 debug mode run)
+	*/
+}
